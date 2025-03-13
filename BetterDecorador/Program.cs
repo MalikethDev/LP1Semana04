ea4755b4a9befb4e53f4e2c3d3237a0d9dcf3b51
Rodrigo <rodrigogomes091997@gmail.com>
2025-03-13 13:10:30 +0000

    Char appears 3 times

---
+++ BetterDecorador/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BetterDecorador
+{
+    public class Program
+    {
+        private static string Decor (string s , char dec, int i)
+        {
+            int count = 3;
+            return $"{dec}{dec}{dec} {s} {dec}{dec}{dec} {count}";
+        }
+        public static void Main ( string [] args )
+        {
+            Console.WriteLine (Decor("Good Morning!" , '*', 3));
+            Console.WriteLine ( Decor ("Good Evening" , '.', 3));
+        }
+    }
+}
+
